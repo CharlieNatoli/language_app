7482cff92bdfa072017343b32b0a2876fa766d43
Charlie Natoli <charlienatoli@MacBookPro.AN-810-AP-I-AC>
2025-02-10 22:28:25 -0800

    formatting

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -9,22 +9,29 @@ const AppHeader = ({ OnNewTopic }: HeaderProps) => {
     Header: {
       top: 0,
       left: 0,
+      position: "relative",
       width: "100%",
-      height: "60px",
-      boxShadow: "0 2px 4px rgba(0,0,0,0.1)",
-      display: "flex",
-      alignItems: "center",
-      padding: "0 20px",
+      height: " 80px",
+      // width: "100%",
+      // height: "60px",
+      // boxShadow: "0 2px 4px rgba(0,0,0,0.1)",
+      // display: "flex",
+      // alignItems: "center",
+      // padding: " 20px",
     },
     title: {
+      position: "absolute",
+      top: "20px",
+      left: "20px",
       fontSize: "20px",
       fontWeight: "bold",
-      margin: 0,
+      // margin: 0,
     },
     buttonContainer: {
       position: "absolute",
       left: "50%",
-      transform: "translateX(-50%)",
+      top: "50%",
+      transform: "translate(-50%, -50%)",
     },
     button: {
       padding: "8px 16px",
@@ -33,14 +40,14 @@ const AppHeader = ({ OnNewTopic }: HeaderProps) => {
       border: "none",
       borderRadius: "4px",
       cursor: "pointer",
-      fontSize: "16px",
+      fontSize: "26px",
       // transition: 'background-color 0.2s ease'
     },
   };
 
   return (
     <div style={styles.Header}>
-      <h1 style={styles.title}>Charlie's Language App</h1>
+      <div style={styles.title}>Charlie's Language App</div>
       <div style={styles.buttonContainer}>
         <button style={styles.button} onClick={OnNewTopic}>
           New Topic
