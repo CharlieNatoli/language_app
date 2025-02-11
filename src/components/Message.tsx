3723a0a79aa809438f79137a7acc369f37077bf4
Charlie Natoli <charlienatoli@MacBookPro.AN-810-AP-I-AC>
2025-02-10 20:39:20 -0800

    clean up message

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,12 +1,5 @@
 export type MessageType = "user" | "ai";
 
-export interface HumanMessageCommentary {
-  id: number;
-  word_choice: string | null;
-  style: string | null;
-  grammar: string | null;
-}
-
 export interface Message {
   id: number;
   type: MessageType;
