e13060cc39f87ba8c276b37b18e94fc255fda115
Charlie Natoli <charlienatoli@Charlies-MacBook-Pro.local>
2025-02-11 17:43:21 -0800

    text submit box

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import ConversationPanel from "./components/ConversationPanel";
 import FeedbackPanel from "./components/FeedbackPanel";
 import AppHeader from "./components/AppHeader/AppHeader";
 import Loader from "./components/Loader";
-import TextSubmitBox from "./components/TextSubmitBox";
+import TextSubmitBox from "./components/TextSubmitBox/TextSubmitBox";
 
 import { BrowserRouter, Routes, Route } from "react-router";
 import { useState, SyntheticEvent, useEffect, useRef } from "react";
